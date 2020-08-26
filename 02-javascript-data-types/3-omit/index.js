92aec061052082d5ff2b52f313f73fc88445c370
NikAndre <nik-13@ya.ru>
2020-08-26 20:04:08 +0300

    задание 2.3

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  const args = [...fields]
+  const newArr2 = []
+  for (o = 0 ; o < Object.entries(obj).length ; o++ ){
+    for(j = 0 ; j < args.length ; j++ ){
+      if(Object.entries(obj)[o][0] !== args[j]){
+        newArr2.push(Object.entries(obj)[o])
+      }
+    }
+  }
+  return Object.fromEntries(newArr2)
 };
